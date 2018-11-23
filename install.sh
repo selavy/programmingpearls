5deac462143a333d4384b2888fd07be6be3771ae
Peter Lesslie <pclesslie@gmail.com>
2018-11-23 13:35:50 -0800

    Begin install instructions for VM

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt install -y vim
+apt install -y git
+apt install -y build-essential
+apt install -y python
+
+# # Ninja BUild
+# git clone git://github.com/ninja-build/ninja.git && cd ninja
+# git checkout release
+# cat Re
+# cat README
+# ./configure.py --bootstrap
+# sudo cp ninja /usr/local/bin/
+# sudo cp ./misc/bash-completion /etc/bash_completion.d/
+
+apt install -y cmake
+apt install -y htop
+
