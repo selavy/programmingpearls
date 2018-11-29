1ce4a5f998efdc38b43f6a0387171f74d2800145
Peter Lesslie <pclesslie@gmail.com>
2018-11-29 15:49:03 -0800

    Python script to generate random unique inputs

---
+++ column_1/gen_numbers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+
+
+if __name__ == '__main__':
+    M = 10000000
+    if len(sys.argv) < 2:
+        N = 100
+    elif len(sys.argv) == 2:
+        N = int(sys.argv[1])
+    else:
+        sys.stderr.write(f"Usage: {sys.argv[0]} <NUM>\n")
+        sys.exit(0)
+
+    if N > M:
+        raise Exception(f"{N} must be lower than {M}")
+
+    vals = np.random.choice(range(M), N, replace=False)
+    for i in vals:
+        print(i)
+
