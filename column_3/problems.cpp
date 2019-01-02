b0625b7d1f3af66fc420c453164df049efcfdd2b
Peter Lesslie <pclesslie@gmail.com>
2019-01-01 19:00:03 -0800

    Remove extraneous prints

--- column_3/problems.cpp
+++ column_3/problems.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <cstdlib>
+#include <cstring>
 
 namespace ch3 {
 
@@ -10,57 +12,37 @@ using Real = double;
 template <size_t K>
 using Reals = std::array<Real, K>;
 
-template <size_t K>
-void real_print(const Reals<K>& r, bool newline=true)
-{
-    for (auto c: r) {
-        std::cout << c << " ";
-    }
-    if (newline) {
-        std::cout << "\n";
-    }
-}
-
+// TODO(peter): I think I should be able to vectorize this
 template <int K, int M>
 Reals<M> recurrence(Reals<K> as, Reals<K+1> cs) noexcept
 {
     Reals<M> output;
-
-    // TEMP
-    for (auto& c: output) {
-        c = 0.0;
-    }
-
-    // TODO(peter): change to a memcpy
-    for (int i = 0; i < K; ++i) {
-        output[i] = as[i];
-    }
-
+    memcpy(&output[0], &as[0], sizeof(as[0])*K);
     for (int n = K; n < M; ++n) {
-        // std::cout << "Begin iteration: " << n + 1 << "\n";
-        // std::cout << "A: "; real_print(as);
-        // std::cout << "C: "; real_print(cs);
-        // std::cout << "O: "; real_print(output);
-
         // Calculate the new value
         Real r = cs[K];
         for (int i = 0; i < K; ++i) {
             r += as[K-i-1]*cs[i];
         }
-
         // TODO(peter): fold propogate into the calculate new value stage
         // Propogate the values backward
         for (int i = 0; i < K-1; ++i) {
             as[i] = as[i+1];
         }
         as[K-1] = r;
-
         output[n] = r;
     }
-
-    // std::cout << "Final Result\nO: "; real_print(output);
-
     return output;
+}
+
+// TODO(peter): definitely should be able to vectorize this one
+void rec2(int K, int M, double* a, const double* c) noexcept {
+    for (int n = K; n < M; ++n) {
+        a[n] = c[K];
+        for (int i = 0; i < K; ++i) {
+            a[n] += a[n-i-1]*c[i];
+        }
+    }
 }
 
 }
@@ -81,24 +63,6 @@ TEST_CASE("Problem #2", "k-th order linear recurrence")
         ch3::Reals<K>   as = { 2 };
         ch3::Reals<K+1> cs = { 2, 1 };
         auto output = ch3::recurrence<K, M>(as, cs);
-        // std::cout << "\n\nFinal Result:\n";
-        // std::cout << "As    : ";
-        // for (auto i: as) {
-        //     std::cout << i << " ";
-        // }
-        // std::cout << "\n";
-
-        // std::cout << "Cs    : ";
-        // for (auto i: cs) {
-        //     std::cout << i << " ";
-        // }
-        // std::cout << "\n";
-
-        // std::cout << "Output: ";
-        // for (auto i: output) {
-        //     std::cout << i << " ";
-        // }
-        // std::cout << "\n";
 
         auto c_1 = cs[0];
         auto c_2 = cs[1];
@@ -112,6 +76,15 @@ TEST_CASE("Problem #2", "k-th order linear recurrence")
         REQUIRE(output[3] == a_4);
 
         REQUIRE(output == ch3::Reals<M>{2, 5, 11, 23});
+
+        // Version #2
+        double A[M];
+        memcpy(&A[0], &as[0], sizeof(as[0])*as.size());
+        ch3::rec2(K, M, &A[0], &cs[0]);
+        REQUIRE(A[0] ==  2);
+        REQUIRE(A[1] ==  5);
+        REQUIRE(A[2] == 11);
+        REQUIRE(A[3] == 23);
     }
 
     SECTION("Harder case")
@@ -143,5 +116,16 @@ TEST_CASE("Problem #2", "k-th order linear recurrence")
 
         auto a_6 = c_1*a_5 + c_2*a_4 + c_3*a_3 + c_4;
         REQUIRE(out[5] == a_6);
+
+        // Version #2
+        double A[M];
+        memcpy(&A[0], &a[0], sizeof(a[0])*a.size());
+        ch3::rec2(K, M, &A[0], &c[0]);
+        REQUIRE(A[0] == a_1);
+        REQUIRE(A[1] == a_2);
+        REQUIRE(A[2] == a_3);
+        REQUIRE(A[3] == a_4);
+        REQUIRE(A[4] == a_5);
+        REQUIRE(A[5] == a_6);
     }
 }
