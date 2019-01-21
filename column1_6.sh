4c455310a9874502c6592c42313f7ec4f41c3bdd
Peter Lesslie <pclesslie@gmail.com>
2019-01-21 11:19:59 -0800

    duplicates

--- column1_6.sh
+++ column1_6.sh
@@ -1,18 +1,16 @@
 #/bin/bash
 
 rm -f output*.txt
-# rm -f input.txt
+rm -f input.txt
 make -C build/
 make -C optimize_build/
 
-# echo "Generating input..."
-# ./column_1/gen_numbers_with_dups.py > input.txt
+echo "Generating input..."
+./column_1/gen_numbers_with_dups.py > input.txt
 
-cat input.txt | ./build/column1_bitmap_sort_duplicates > output1.txt
-# cat input.txt | ./optimize_build/column1_bitmap_sort_duplicates > output1.txt
-cat input.txt | sort -n > output2.txt
+# cat input.txt | ./build/column1_bitmap_sort_duplicates > output1.txt
+time cat input.txt | ./optimize_build/column1_bitmap_sort_duplicates > output1.txt
+time cat input.txt | sort -n > output2.txt
 
-head -n 20 output1.txt > output1_short.txt
-head -n 20 output2.txt > output2_short.txt
-diff output1_short.txt output2_short.txt
+diff output1.txt output2.txt
 
--- column_1/bitmap_sort_duplicates.cpp
+++ column_1/bitmap_sort_duplicates.cpp
@@ -9,18 +9,38 @@
 constexpr int MaxValue = 10000000;
 constexpr int Elements = MaxValue / 2; // using 4-bits per number
 
-struct {
+// #define DESIGN_1
+// #define DESIGN_2
+#define DESIGN_3
+
+#ifdef DESIGN_1
+
+struct EE {
     uint8_t lo:4; // even
     uint8_t hi:4; // odd
-} cnts[MaxValue / 2];
+};
+static EE cnts[Elements];
+
+#elif defined(DESIGN_2) || defined(DESIGN_3)
+
+static uint8_t cnts[Elements];
+
+#else
+
+    #error "Must pick a design"
+
+#endif
 
 
 int main(int argc, char** argv) {
     char input[256];
     memset(&cnts[0], 0, sizeof(cnts));
+
     while (fgets(&input[0], sizeof(input), stdin) != NULL) {
         const int val = atoi(&input[0]);
         const int idx = val / 2;
+
+#ifdef DESIGN_1
         if (val % 2 == 0) {
             ++cnts[idx].lo;
             assert(cnts[idx].lo <= 10 && "More than 10 duplicates!");
@@ -28,13 +48,39 @@ int main(int argc, char** argv) {
             ++cnts[idx].hi;
             assert(cnts[idx].hi <= 10 && "More than 10 duplicates!");
         }
+#elif defined(DESIGN_2)
+        if (val % 2 == 0) {
+            cnts[idx] += 1u << 0;
+        } else {
+            cnts[idx] += 1u << 4;
+        }
+        assert(((cnts[idx] >> 0) & 0x0F) <= 10 && "More than 10 duplicates!");
+        assert(((cnts[idx] >> 4) & 0x0F) <= 10 && "More than 10 duplicates!");
+#elif defined(DESIGN_3)
+        uint8_t lo = (cnts[idx] >> 0) & 0x0F;
+        uint8_t hi = (cnts[idx] >> 4) & 0x0F;
+        if (val % 2 == 0) {
+            ++lo;
+        } else {
+            ++hi;
+        }
+        cnts[idx] = (hi << 4) | (lo << 0);
+#endif
     }
 
-    for (int i = 0; i < MaxValue/2; ++i) {
-        for (int p = 0; p < cnts[i].lo; ++p) {
+    for (int i = 0; i < Elements; ++i) {
+
+#ifdef DESIGN_1
+        int lo = cnts[i].lo;
+        int hi = cnts[i].hi;
+#elif defined(DESIGN_2) || defined(DESIGN_3)
+        int lo = (cnts[i] >> 0) & 0x0F;
+        int hi = (cnts[i] >> 4) & 0x0F;
+#endif
+        for (int p = 0; p < lo; ++p) {
             printf("%d\n", 2*i);
         }
-        for (int p = 0; p < cnts[i].hi; ++p) {
+        for (int p = 0; p < hi; ++p) {
             printf("%d\n", 2*i+1);
         }
     }
